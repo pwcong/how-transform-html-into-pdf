277d7fd80fe5315f91bf5e834c4becc8725c8ced
pwcong <pwcong@foxmail.com>
2017-03-25 14:50:56 +0800

    [+] add live demo

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # how-transform-html-into-pdf
 A example about how to transform HTML into PDF
 
+[Demo预览](http://pwcong.me/how-transform-html-into-pdf/)
+
 # Require
 * [html2canvas](https://github.com/niklasvh/html2canvas)
 * [jspdf](https://github.com/MrRio/jsPDF)
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 document.getElementById("btn-html2canvas").onclick = function(){
 
+    // 将 id 为 content 的 div 渲染成 canvas
     html2canvas(document.getElementById("content"), {
+
+        // 渲染完成时调用，获得 canvas
         onrendered: function(canvas) {
 
+            // 从 canvas 提取图片数据
             var imgData = canvas.toDataURL('image/jpeg');
 
             var doc = new jsPDF("p", "mm", "a4");
