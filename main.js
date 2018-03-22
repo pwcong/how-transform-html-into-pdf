d60631d7355be9fae434c2471fff728ff07c567d
奈何 <690372513@qq.com>
2018-03-22 12:10:32 +0800

    update to adape es6 and changeable width and height

--- main.js
+++ main.js
@@ -1,35 +1,21 @@
 document.getElementById("btn-html2canvas").onclick = function(){
 
     // 将 id 为 content 的 div 渲染成 canvas
-    html2canvas(document.getElementById("content"), {
-
+    let parent = document.getElementById("content")
+    let width = parent.offsetWidth;
+    let height = parent.offsetHeight
+    html2canvas(document.getElementById("content"))
+        ,then(canvas => {
         // 渲染完成时调用，获得 canvas
-        onrendered: function(canvas) {
 
             // 从 canvas 提取图片数据
             var imgData = canvas.toDataURL('image/jpeg');
 
-            var doc = new jsPDF("p", "mm", "a4");
-            //                               |
-            // |—————————————————————————————|                     
-            // A0 841×1189                           
-            // A1 594×841                            
-            // A2 420×594                            
-            // A3 297×420                            
-            // A4 210×297                            
-            // A5 148×210                            
-            // A6 105×148                            
-            // A7 74×105                             
-            // A8 52×74                              
-            // A9 37×52                              
-            // A10 26×37             
-            //     |——|———————————————————————————|
-            //                                 |——|——|
-            //                                 |     |      
-            doc.addImage(imgData, 'JPEG', 0, 0,210,297);
+            var doc = new jsPDF("p", "mm", [width, height]);
+
+            doc.addImage(imgData, 'JPEG', 0, 0,width, height);
 
             doc.save('content.pdf');
-        }
-    });
-
+        
+    })
 }
